12e7ea2c63fcaffcb6e345665f581afa2facf328
bob <robertbalonek@gmail.com>
2021-06-27 19:04:56 -0700

    Work / Intern

---
+++ src/components/BlueOrca/BlueOrca.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import "./BlueOrca.css";
+import { Button } from "react-bootstrap";
+
+import { Carousel } from "react-bootstrap";
+import ReactPlayer from "react-player";
+
+import orcaOne from "../../assets/BlueOrca.jpg";
+import orcaTwo from "../../assets/BlueOrca2.jpg";
+import orcaThree from "../../assets/BlueOrca3.jpg";
+import orcaFour from "../../assets/BlueOrca4.jpg";
+import orchaNews from "../../assets/Screen Shot 2021-06-27 at 5.31.35 PM.png";
+
+export default function BlueOrca() {
+  const goToSite = () => {
+    window.open(
+      "https://www.amazon.com/Blue-Orca-Straw-Bracelet-Openable/dp/B092CG178W/ref=sr_1_2?dchild=1&keywords=blue+orca&qid=1624571764&sr=8-2",
+      "_blank"
+    );
+  };
+
+  return (
+    <div className="blue-orcha__container">
+      <div className="blue-orcha__card">
+        <h1>Blue Orca</h1>
+        <p>June 2020</p>
+        <p>For profit organization</p>
+        <p>TITLE: Co-Founder and CFO </p>
+        <Carousel playing={false} loop="false">
+          <Carousel.Item>
+            <img className="orcha-img" src={orcaOne} alt={orcaOne} />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img className="orcha-img" src={orcaTwo} alt={orcaTwo} />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img className="orcha-img" src={orcaThree} alt={orcaThree} />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img className="orcha-img" src={orcaFour} alt={orcaFour} />
+          </Carousel.Item>
+
+          <Carousel.Item interval={60000}>
+            <ReactPlayer
+              style={{ textAlign: "-webkit-center", marginBottom: "20px" }}
+              width="50%"
+              height="500px"
+              // playing={true}
+              controls={true}
+              url="https://res.cloudinary.com/bobalobbadingdong/video/upload/v1624840455/Arnav/BlueOrca_a9gsi7.mp4"
+            />
+          </Carousel.Item>
+        </Carousel>
+
+        <div className="blue-orcha__text">
+          <p>
+            LaunchX alumni (formerly an MIT program) where students launch their
+            own startups.
+          </p>
+          <p>
+            Working in a team of 5, launched a new product, Blue Orca, a
+            reusable straw in a fashionable bracelet case to help keep our
+            oceans plastic free.
+          </p>
+          <p>
+            Responsible for Marketing research of the product. Prepared business
+            plans and got estimates from suppliers in G.China and Taiwan for
+            manufacturing. 
+          </p>
+          <p>
+            Sales of $1000+. Ran Indiegogo campaign ($700+). Now sold on Amazon
+            for $14.99.
+          </p>
+          <p>
+            Our product was endorsed by UC professor and it was featured in a
+            Newspaper (100,000+ readers)
+          </p>
+          <Button onClick={goToSite}>Buy Now</Button>
+        </div>
+        <img style={{ marginTop: "20px" }} alt="paper" src={orchaNews} />
+      </div>
+    </div>
+  );
+}
--- src/work/WorkExperience.jsx
+++ src/work/WorkExperience.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import "./WorkExperience.css";
+import BlueOrca from "../components/BlueOrca/BlueOrca";
+import ResearchItem from "../components/ResearchItem/ResearchItem";
+
+import Syracuse from "../assets/Syracuse.jpeg";
+import Foothill from "../assets/Foothill.jpeg";
+import Book from "../assets/Book.jpg";
 
 export default function WorkExperience() {
   return (
-    <div>
-      <h1>Work Exp</h1>
+    <div className="work-experience__container">
+      <h1 style={{ textAlign: "center" }}>Work Experience</h1>
+      <hr />
+      <BlueOrca />
+      <hr />
+      <h1 style={{ textAlign: "center" }}>Research Work</h1>
+      <hr />
+      <ResearchItem
+        title="Research Intern"
+        where="Soccer Analytics Club at Syracuse University"
+        logo={Syracuse}
+        mission="working with university students to research using player ratings for lineups to predict team winning probability to maximize revenue."
+      />
+
+      <ResearchItem
+        title="Independent Researcher"
+        where="Foothill College"
+        logo={Foothill}
+        mission="Working independently under a professor to research on the importance of sub-competitions and overall competitions for leagues to get maximum economics benefits."
+      />
+
+      <ResearchItem
+        title="Author"
+        where="Economics of Soccer and COVID-19"
+        logo={Book}
+        mission="Wrote and self-published a book on the economic aspects of the soccer industry and impacts of Covid-19. Distributed to underprivileged kids in Africa and Peru. In this book, we dive into the economic impact of covid-19 and have a deeper look at what goes around in the backend of the soccer industry. We also take a deeper look at how fundamental economics concepts influence soccer."
+        link="https://www.amazon.com/Economics-Soccer-Covid-19-Arnav-Jain/dp/B08RT6FB64/ref=sr_1_2?dchild=1&keywords=economics+of+soccer+and+covid+19&qid=1618705536&sr=8-2"
+      />
     </div>
   );
 }
