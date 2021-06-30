23ad304889fea41cc13de69ea11f12cfee7aff66
bob <robertbalonek@gmail.com>
2021-06-29 17:52:58 -0700

    Mobiel Friendly

--- src/components/Academic/Academic.jsx
+++ src/components/Academic/Academic.jsx
@@ -4,7 +4,7 @@ import "./Academic.css";
 export default function Academic(props) {
   return (
     <div className="academic__container">
-      <h4>{props.title}</h4>
+      <h4 className="academic__title">{props.title}</h4>
       {props.content.map((thing) => (
         <p>{thing}</p>
       ))}
