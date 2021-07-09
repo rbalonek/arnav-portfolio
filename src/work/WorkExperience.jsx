04ad2d2e09381e9226f319c49b0c13ace3d96ed1
bob <robertbalonek@gmail.com>
2021-07-09 12:55:54 -0700

    Lighter Theme Done

--- src/work/WorkExperience.jsx
+++ src/work/WorkExperience.jsx
@@ -11,26 +11,31 @@ import Award from "../components/Award/Award";
 export default function WorkExperience() {
   return (
     <div className="work-experience__container">
-      <h1 className="section-title">Work Experience</h1>
-      <hr />
+      <h1
+        style={{ color: "white", marginTop: "0px" }}
+        className="section-title"
+      >
+        Work Experience
+      </h1>
       <BlueOrca />
-      <hr />
-      <h1 className="section-title">Research Work</h1>
-      <hr />
+      <h1
+        style={{ color: "white", marginTop: "0px" }}
+        className="section-title"
+      >
+        Research Work
+      </h1>
       <ResearchItem
         title="Research Intern"
         where="Soccer Analytics Club at Syracuse University"
         logo={Syracuse}
         mission="working with university students to research using player ratings for lineups to predict team winning probability to maximize revenue."
       />
-
       <ResearchItem
         title="Independent Researcher"
         where="Foothill College"
         logo={Foothill}
         mission="Working independently under a professor to research on the importance of sub-competitions and overall competitions for leagues to get maximum economics benefits."
       />
-
       <ResearchItem
         title="Author"
         where="Economics of Soccer and COVID-19"
@@ -38,9 +43,14 @@ export default function WorkExperience() {
         mission="Wrote and self-published a book on the economic aspects of the soccer industry and impacts of Covid-19. Distributed to underprivileged kids in Africa and Peru. In this book, we dive into the economic impact of covid-19 and have a deeper look at what goes around in the backend of the soccer industry. We also take a deeper look at how fundamental economics concepts influence soccer."
         link="https://www.amazon.com/Economics-Soccer-Covid-19-Arnav-Jain/dp/B08RT6FB64/ref=sr_1_2?dchild=1&keywords=economics+of+soccer+and+covid+19&qid=1618705536&sr=8-2"
       />
-      <hr />
-      <h1 className="section-title">Awards</h1>
-      <hr />
+
+      <h1
+        style={{ color: "white", marginTop: "0px" }}
+        className="section-title"
+      >
+        Awards
+      </h1>
+
       <Award />
     </div>
   );
