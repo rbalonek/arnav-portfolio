18880a82f3607317ab9172853b1b546607932240
bob <robertbalonek@gmail.com>
2021-07-14 20:09:01 -0700

    WIP LANDING SCREEN

--- src/components/New Components/AcaemicSlider/AcaemicSlider.jsx
+++ src/components/New Components/AcaemicSlider/AcaemicSlider.jsx
@@ -5,29 +5,31 @@ import { Carousel, CarouselItem } from "react-bootstrap";
 export default function AcaemicSlider() {
   return (
     <div className="academic-slider__container">
-      <Carousel
-        controls={true}
-        indicators={true}
-        className="academic-slider__carousel "
-      >
-        <CarouselItem>
-          <h3>Social Entrepreneur</h3>
-        </CarouselItem>
-        <CarouselItem>
-          <h3>Academics</h3>
-          <p>Unweighted GPA 4.0</p>
-          <p>SAT 1550</p>
-        </CarouselItem>
-        <CarouselItem>
-          <h3>Founder/CEO of Thunder Shots</h3>
-        </CarouselItem>
-        <CarouselItem>
-          <h3>Competitive Soccer Player</h3>
-        </CarouselItem>
-        <CarouselItem>
-          <h3>Author</h3>
-        </CarouselItem>
-      </Carousel>
+      <div className="academic-slider__background">
+        <Carousel
+          controls={true}
+          indicators={true}
+          className="academic-slider__carousel "
+        >
+          <CarouselItem>
+            <h3>Social Entrepreneur</h3>
+          </CarouselItem>
+          <CarouselItem>
+            <h3>Academics</h3>
+            <p>Unweighted GPA 4.0</p>
+            <p>SAT 1550</p>
+          </CarouselItem>
+          <CarouselItem>
+            <h4>Founder/CEO of Thunder Shots</h4>
+          </CarouselItem>
+          <CarouselItem>
+            <h3>Competitive Soccer Player</h3>
+          </CarouselItem>
+          <CarouselItem>
+            <h3>Author</h3>
+          </CarouselItem>
+        </Carousel>
+      </div>
     </div>
   );
 }
--- src/components/New Components/Landing/Landing.jsx
+++ src/components/New Components/Landing/Landing.jsx
@@ -3,23 +3,39 @@ import "./Landing.css";
 
 import Headshot from "../../../assets/ArnavHeadshot.jpg";
 import AcaemicSlider from "../AcaemicSlider/AcaemicSlider";
+import { Button } from "react-bootstrap";
 
 export default function Landing() {
   return (
     <div className="landing__container">
       <div className="landing__left-third">
-        <p>
-          A RISING SENIOR WHO WOULD LIKE TO MAKE AN IMPACT IN THE LIFE OF
-          UNDERPRIVILEDGED KIDS AROUND THE WORLD.
-        </p>
-        <br />
-        <p>Be Worthy, Start Small and Make it BIG.</p>
+        <div className="left-third__text-container">
+          <h4 style={{ textTransform: "lowercase" }}>
+            <span style={{ textTransform: "uppercase" }}>A</span> RISING SENIOR
+            WHO WOULD LIKE TO MAKE AN IMPACT IN THE LIFE OF UNDERPRIVILEDGED
+            KIDS AROUND THE WORLD.
+          </h4>
+          <br />
+          <h4 style={{ color: "#0077D7" }}>
+            Be Worthy, Start Small and Make it BIG.
+          </h4>
+        </div>
       </div>
+
       <div className="landing__middle-third">
         <img className="landing__image" src={Headshot} alt="img" />
       </div>
+
       <div className="landing__right-third">
-        <AcaemicSlider />
+        <div className="landing__carousel-holder">
+          <div className="landing__carousel-holder__btn-split">
+            <Button style={{ position: "relative", bottom: "50px" }}>
+              Transcripts
+            </Button>
+            <Button>Resume</Button>
+          </div>
+          <AcaemicSlider />
+        </div>
       </div>
     </div>
   );
