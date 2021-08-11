9a3f246bc6e697d70da92fb2306b28bd5bceaf46
Robert Balonek <robertbalonek@gmail.com>
2021-08-11 11:38:13 -0700

    Social

--- src/components/New Components/SocialImpact/SocialImpact.jsx
+++ src/components/New Components/SocialImpact/SocialImpact.jsx
@@ -16,19 +16,25 @@ export default function SocialImpact() {
 
       <div className="social-impact__content-split">
         <div className="social-impact__left-one-third">
-          <h4>THE THUNDER SHOTS</h4>
-          <p> FOUNDER AND CEO</p>
+          <h4 className="social-impact__title">THE THUNDER SHOTS</h4>
+          <h4 className="social-impact__sub-title">CO-FOUNDER AND CEO</h4>
 
-          <p>
+          <p className="social-impact__text">
             Pursuing my passion to serve community and under privileged kids
             across the world.
           </p>
 
-          <p>
-            <span style={{ color: "black" }}>
-              Thethundershots.org Featured in Almaden Times
-            </span>
+          <p className="social-impact__text-link">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="Thethundershots.org"
+              alt="thundershots"
+            >
+              <span style={{ color: "#3984cf" }}>Thethundershots.org</span>
+            </a>
           </p>
+          <p className="social-impact__text">Featured in Almaden Times</p>
         </div>
         <div className="social-impact__right-two-thirds">
           <SocialImpactSlider />
