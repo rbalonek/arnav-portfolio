705d6935c74a9c5e03015196c269b718dd81bd19
Robert Balonek <robertbalonek@gmail.com>
2021-08-11 13:20:16 -0700

    Fixed Leadership

--- src/components/New Components/Leadership/LeadershipOutside/LeadershipOutside.jsx
+++ src/components/New Components/Leadership/LeadershipOutside/LeadershipOutside.jsx
@@ -14,108 +14,133 @@ export default function LeadershipOutside() {
 
       <div className="leadership-outside__box left-a-bit">
         <p className="leadership-outside__title">
-          Blue Orca Co Founder and CFO
+          Co-Founder and CFO: Blue Orca
         </p>
-        <p style={{ fontWeight: "bold" }} className="leadership-outside__text">
-          “A reusable straw with a path breaking design” to keep our oceans
-          plastic free.
-        </p>
-        <img
-          className="leadership-outside__orcha-pics"
-          src={OrchaOne}
-          alt={OrchaOne}
-        />
-        <img
-          className="leadership-outside__orcha-pics"
-          src={OrchaTwo}
-          alt={OrchaTwo}
-        />
 
-        <p className="leadership-outside__text">
-          From ‘Conceptualization’ to ‘Bringing product to Market’. As part of
-          LaunchX program.
-        </p>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://drive.google.com/file/d/1-t9KoK-CWhITTwIjoCBQ85q8IBXcEkbi/view?usp=sharing"
-        >
-          <p style={{ textDecoration: "underline" }}>VIDEO</p>
-        </a>
+        <div className="leadership-outside__box-split">
+          <div className="leadership-outside__box-left">
+            <p
+              style={{ fontWeight: "bold" }}
+              className="leadership-outside__text"
+            >
+              “A reusable straw with a path breaking design” to keep our oceans
+              plastic free.
+            </p>
+
+            <p className="leadership-outside__text">
+              From ‘Conceptualization’ to ‘Bringing product to Market’. As part
+              of LaunchX program.
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://drive.google.com/file/d/1-t9KoK-CWhITTwIjoCBQ85q8IBXcEkbi/view?usp=sharing"
+            >
+              <p style={{ textDecoration: "underline" }}>VIDEO</p>
+            </a>
+          </div>
+          <div className="leadership-outside__box-right">
+            <img
+              className="leadership-outside__orcha-pics"
+              src={OrchaOne}
+              alt={OrchaOne}
+            />
+            <img
+              className="leadership-outside__orcha-pics"
+              src={OrchaTwo}
+              alt={OrchaTwo}
+            />
+          </div>
+        </div>
       </div>
 
       <div className="leadership-outside__box">
         <p className="leadership-outside__title">
           CA State President: Paper Bridges
         </p>
-        <p style={{ fontWeight: "bold" }} className="leadership-outside__text">
-          “Started 18 Chapters in California and paired them with Orphanages
-          across the Globe”
-        </p>
-        <img
-          className="leadership-outside__paper-pic"
-          src={Paper}
-          alt={Paper}
-        />
+        <div className="bridges__split">
+          <div className="bridges__left">
+            <p
+              style={{ fontWeight: "bold" }}
+              className="leadership-outside__text down-a-bit"
+            >
+              “Started 18 Chapters in California and paired them with Orphanages
+              across the Globe”
+            </p>
+          </div>
 
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.paper
-      bridges.org/"
-        >
-          <p className="leadership-outside__text">
-            https://www.paper bridges.org/
-          </p>
-        </a>
-        <p>“A Thunder Shots Partner”</p>
+          <div className="bridges__right">
+            <img
+              className="leadership-outside__paper-pic"
+              src={Paper}
+              alt={Paper}
+            />
+
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.paper
+        bridges.org/"
+            >
+              <p className="leadership-outside__text">
+                https://www.paper bridges.org/
+              </p>
+            </a>
+            <p>“A Thunder Shots Partner”</p>
+          </div>
+        </div>
       </div>
 
       <div
         className="leadership-outside__box"
         style={{
-          height: "570px",
+          // height: "570px",
           marginBottom: "20px",
         }}
       >
         <p className="leadership-outside__title">
           Vice President Marketing at KoreKloud
         </p>
-        <p style={{ fontWeight: "bold" }} className="leadership-outside__text">
-          “Manage Social Media Platforms to Engage 700+ students for over 25
-          courses”
-        </p>
 
-        <p
-          style={{ fontWeight: "bold", marginBottom: "30px" }}
-          className="leadership-outside__text"
-        >
-          “Oversee development of courses and management of workshops”
-        </p>
-        <div style={{ position: "relative", bottom: "100px" }}>
-          <KoreKloudCarousel />
+        <div className="kore-split">
+          <div className="kore-left">
+            <p
+              style={{ fontWeight: "bold" }}
+              className="leadership-outside__text"
+            >
+              “Manage Social Media Platforms to Engage 700+ students for over 25
+              courses”
+            </p>
+
+            <p
+              style={{ fontWeight: "bold", marginBottom: "30px" }}
+              className="leadership-outside__text"
+            >
+              “Oversee development of courses and management of workshops”
+            </p>
+          </div>
+          <div className="kore-right">
+            <div style={{ position: "relative", bottom: "120px" }}>
+              <KoreKloudCarousel />
+            </div>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.korekloud.org"
+            >
+              <p
+                className="change-for-mobile"
+                style={{
+                  textDecoration: "underline",
+                  position: "relative",
+                  // bottom: "100px",
+                }}
+              >
+                https://www.korekloud.org
+              </p>
+            </a>
+          </div>
         </div>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.korekloud.org"
-        >
-          <p
-            style={{
-              textDecoration: "underline",
-              position: "relative",
-              bottom: "100px",
-            }}
-          >
-            https://www.korekloud.org
-          </p>
-        </a>
-        <p
-          className="move-up-responsive"
-          // style={{ position: "relative", bottom: "100px" }}
-        >
-          “A Thunder Shots Partner”
-        </p>
       </div>
     </div>
   );
