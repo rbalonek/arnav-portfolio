1a67b563db4f09c2e8980c74fc5e3b5cab4f5ceb
Robert Balonek <robertbalonek@gmail.com>
2021-08-11 13:29:09 -0700

    Awards

--- src/components/New Components/Awards/AwardsCarousel/AwardsCarousel.jsx
+++ src/components/New Components/Awards/AwardsCarousel/AwardsCarousel.jsx
@@ -3,28 +3,28 @@ import "./AwardsCarousel.css";
 import { Carousel, CarouselItem } from "react-bootstrap";
 
 import One from "../../../../assets/JvSoccerWider.jpg";
-import Two from "../../../../assets/AwardsPics/IMG_2215.JPG";
+// import Two from "../../../../assets/AwardsPics/IMG_2215.JPG";
 import Three from "../../../../assets/AwardsPics/IMG_2218.JPG";
-import Four from "../../../../assets/AwardsPics/IMG_3632.JPG";
-import Five from "../../../../assets/AwardsPics/IMG_3910.JPG";
+// import Four from "../../../../assets/AwardsPics/IMG_3632.JPG";
+// import Five from "../../../../assets/AwardsPics/IMG_3910.JPG";
 import Six from "../../../../assets/AwardsPics/IMG_4516.jpg";
-import Seven from "../../../../assets/AwardsPics/IMG_4517.jpg";
+// import Seven from "../../../../assets/AwardsPics/IMG_4517.jpg";
 import Eight from "../../../../assets/AwardsPics/IMG_4520.jpg";
 import Nine from "../../../../assets/AwardsPics/IMG_4522.jpg";
-import Ten from "../../../../assets/AwardsPics/IMG_4523.jpg";
+// import Ten from "../../../../assets/AwardsPics/IMG_4523.jpg";
 
 export default function AwardsCarousel() {
   const AwardsImgSlides = [
     One,
-    Two,
+    // Two,
     Three,
-    Four,
-    Five,
+    // Four,
+    // Five,
     Six,
-    Seven,
+    // Seven,
     Eight,
     Nine,
-    Ten,
+    // Ten,
   ];
   return (
     <div className="awards-carousel__container">
