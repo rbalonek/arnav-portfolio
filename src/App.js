c81ab915b9912daf8416397c98064311631738ac
Robert Balonek <robertbalonek@gmail.com>
2021-08-12 13:10:37 -0700

    Landing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Layout from "./layout/Layout.jsx";
 import { Route, Switch } from "react-router-dom";
 import MainContainer from "./containers/MainContainer";
+import MultiPageContainer from "./containers/MultiPageContainer";
 
 function App() {
   return (
     <Layout>
       <Switch>
         <Route path="/">
-          <MainContainer />
+          <MultiPageContainer />
         </Route>
       </Switch>
     </Layout>
